17eca5099adbf39003181b86bb87f8b498ed07e3
MarinaVolkova <mirra.blek@mail.ru>
2022-02-04 21:32:46 +0300

    create a home page

---
+++ src/App.js
@@ -0,0 +1,24 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>header</h1>
+      </header>
+      <main className="App-main">
+        <h2>main</h2>
+        <section> 
+          <h4> section</h4>
+        </section>
+      </main>
+      <footer className="App-footer">
+        <h2>footer</h2>
+      </footer>
+    </div>
+  );
+}
+
+
+export default App;
